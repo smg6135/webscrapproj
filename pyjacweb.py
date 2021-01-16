927cc313323bd18d883b0988c79fa7b5fd1e6c33
smg6135 <smg6135@gmail.com>
2021-01-15 22:20:19 -0500

    basic gui 1st page

--- pyjacweb.py
+++ pyjacweb.py
@@ -1,20 +1,50 @@
+# gui
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import messagebox
 # web scrap
-from re import findall
+import re
 import requests
+import json
 from bs4 import BeautifulSoup
 # pyqt5
 import sys
 #from pyQt5 import QApplication, Qwidget
 
-username = input("whats your summoner id? \n")
+class MyDialog(simpledialog.Dialog):
+    def body(self, master):
+        self.geometry("600x400")
+        tk.Label(master, text="What is your summoner ID ?").grid(row=0)
+
+        self.e1 = tk.Entry(master)
+        self.e1.grid(row=0, column=1)
+        return self.e1 # initial focus
+
+    def apply(self):
+        first = self.e1.get()
+        self.result = first
+
+
+root = tk.Tk()
+root.withdraw()
+user = MyDialog(root, "League Statistics Web Scrapper")
+user.apply
+username = user.result
+print(username)
+
+
+#username = input("whats your summoner id? \n")
 # op.gg, wasted on league request
 urlop = "https://na.op.gg/summoner/userName=" + username
 urlwol = "https://wol.gg/stats/na/" + username + "/"
+urlyour = "https://your.gg/na/profile/" + username
 htmlop = requests.get(urlop).text
 htmlwol = requests.get(urlwol).text
+htmlyour = requests.get(urlyour).text
 # beautiful Soup
 soupop = BeautifulSoup(htmlop, 'html.parser')
 soupwol = BeautifulSoup(htmlwol, 'html.parser')
+soupyour = BeautifulSoup(htmlyour, 'html.parser')
 
 user_rank_solo = soupop.select('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo')
 user_rank_flex = soupop.select('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.sub-tier')
@@ -59,14 +89,14 @@ print(spent_mins[0].text)
 spent_mins = soupwol.select('#time-days')
 print(spent_mins[0].text)
 world_rank = soupwol.select('#rank-world')
-print(world_rank[0].text.strip())
+print(world_rank[0].text.strip().replace(" ", ""))
 
 print('-------------------------------------')
 print('Most played champion')
 print('-------------------------------------')
 print(most_played.find('div').get('title'))
 most_played_kda = most_played.find('div', {'class': "PersonalKDA"}).find('div', {'class': 'KDAEach'}).select('span')
-print(most_played_kda[0].text, most_played_kda[1].text, most_played_kda[2].text, most_played_kda[3].text, most_played_kda[4].text)
+print('K ', most_played_kda[0].text, most_played_kda[1].text,'D ', most_played_kda[2].text, most_played_kda[3].text, 'A ', most_played_kda[4].text)
 
 # 3 recent games but change number than boom more
 user_games = soupop.select('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.RealContent > div > div.Content > div.GameItemList > div.GameItemWrap')
@@ -79,9 +109,14 @@ for i in range(3):
     k = user_games[i].select('div.KDA > span.Kill')
     d = user_games[i].select('div.KDA > span.Death')
     a = user_games[i].select('div.KDA > span.Assist')
-    print(k[0].text, '/', d[0].text, '/', a[0].text)
+    print('K ', k[0].text, ' D ', '/', d[0].text, '/', a[0].text)
 
 
 print('-------------------------------------')
-print('Your total game summary')
+print('Your total game summary, S(best) ~ F(worst)')
 print('-------------------------------------')
+game_summary = soupyour.select('#profileGraphAll')
+data = game_summary[0].get("data-json")
+json_data = json.loads(data)
+for i in range(len(json_data)):
+    print(json_data[i]['label'], ': ', json_data[i]['grade'])
